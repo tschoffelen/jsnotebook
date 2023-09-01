b5cb4b8f0d37e3d985347dbb27842b5151ccc737
Thomas Schoffelen <thomas@schof.co>
2023-09-01 10:54:07 +0100

    feat: add Includable label

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import IncludableLabel from "@/components/IncludableLabel";
 import "./globals.scss";
 import { Inter } from "next/font/google";
 
@@ -12,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <IncludableLabel />
+      </body>
     </html>
   );
 }
